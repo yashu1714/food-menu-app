9070f5aade15cece7c6f2de61689ad5ada97742c
N Yaswanth <115484137+yashu1714@users.noreply.github.com>
2025-12-15 20:07:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,46 @@
+import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
+
+import ProfileMenu from "./components/ProfileMenu";
 import RecipeSearch from "./components/RecipeSearch";
 import FavoritesPage from "./components/FavoritesPage";
 import Login from "./components/Login";
-import CreateAccount from "./pages/CreateAccount"; 
-import ResetPassword from "./pages/ResetPassword";
-import { AuthProvider } from "./context/AuthContext";
-import ProtectedRoute from "./components/ProtectedRoute";
-import "./App.css"; 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-const App = () => {
+import { AuthProvider } from "./context/AuthContext";
+import "./App.css";
+
+function App() {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    document.body.setAttribute("data-theme", theme);
+  }, [theme]);
+
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  };
+
   return (
     <AuthProvider>
-      <Routes>
-        <Route path="/" element={<RecipeSearch />} />
-        <Route path="/favorites" element={<FavoritesPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/create" element={<CreateAccount />} />
-        <Route path="/reset" element={<ResetPassword />} />
-        <Route path="/favorites"element={<ProtectedRoute> <FavoritesPage /></ProtectedRoute> } />
-      </Routes>
-       <ToastContainer position="top-right" theme="colored" />
+      <div className="app-container">
+        {/* NAVBAR */}
+        <div className="navbar">
+          <ProfileMenu theme={theme} toggleTheme={toggleTheme} />
+        </div>
+
+        {/* ROUTES */}
+        <Routes>
+          <Route path="/" element={<RecipeSearch />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+          <ToastContainer position="top-right" autoClose={2000} theme="colored" />
+      </div>
     </AuthProvider>
   );
-};
+}
 
 export default App;
-//cd "C:\Users\yaswa\OneDrive\Pictures\Desktop\Food Menu\food-menu-app"
